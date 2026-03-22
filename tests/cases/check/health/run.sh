5acd5eb2b1b987ff5221e4d1d451de4cc711ce1a
OGAWA Keiji <70509917+kibi2@users.noreply.github.com>
2026-03-22 09:07:48 +0900

    ci: add local : LC_ALL=C sort out-actual.txt > gen.txt

--- tests/cases/check/health/run.sh
+++ tests/cases/check/health/run.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-# Open a CSV file, save it immediately, and verify the file contents.
-# This also serves as a check for idempotency.
 set -eu
 
 NVIM_TIRENVI_DEV=1 nvim --headless -u NONE -n -S run.vim > stdout.txt 2> stderr.txt
 
-sort out-actual.txt > gen.txt
+LC_ALL=C sort out-actual.txt > gen.txt
 mv gen.txt out-actual.txt
